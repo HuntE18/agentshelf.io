464e5bbdd916acbc8bd75242a49aec2ff616ffef
HuntE18 <hunterpadlo@gmail.com>
2026-04-14 10:45:27 -0400

    sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from "next";
 import { prisma } from "@/lib/prisma";
 import { TUTORIALS } from "@/content/tutorials";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = "https://www.agentshelf.io";
 
